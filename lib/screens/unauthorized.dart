f482bf340d1d5a8ae4a53ba507c6c90df166bbfe
bongani.mbele <bongani.mbele@raretag.co.za>
2022-02-22 10:06:57 +0200

    unauthorized screen id

--- lib/screens/unauthorized.dart
+++ lib/screens/unauthorized.dart
@@ -1,6 +1,7 @@
 part of mawa;
 
 class Unauthorized extends StatelessWidget {
+  static const String id = 'Unauthorized';
   String? appName;
   Unauthorized({this.appName});
 
@@ -27,7 +28,7 @@ class Unauthorized extends StatelessWidget {
               children: const [
                 Icon(Icons.error_outline,
                   size: 20.0,),
-                Text('You Are Not Authorized To Utilize This Mobile Application',
+                Text('You Are Not Authorized To Utilize This Application',
                   style: TextStyle(color: Colors.black,fontSize: 20.0),
                   textAlign: TextAlign.center,
                 ),
