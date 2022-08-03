ae4142a53815dc06169807a3c1a50412e3e7834c
Zama Bongani Mbele <70573530+bonganimbele@users.noreply.github.com>
2022-08-03 12:46:06 +0200

    fixed dependency dio exporting error

--- lib/dependencies.dart
+++ lib/dependencies.dart
@@ -4,5 +4,7 @@ export 'package:cupertino_icons/cupertino_icons.dart';
 export 'package:another_flushbar/flushbar.dart';
 export 'package:device_info_plus/device_info_plus.dart';
 export 'package:awesome_dialog/awesome_dialog.dart';
-export 'package:dio/adapter.dart';
-export 'package:dio/adapter_browser.dart';
+export 'package:dio/dio.dart';
+
+// export 'package:dio/adapter.dart';
+// export 'package:dio/adapter_browser.dart';
