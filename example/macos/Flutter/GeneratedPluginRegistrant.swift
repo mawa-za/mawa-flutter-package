9aa3a66beb9ad237904631860b31590f63b7321e
tebogo.mohale <tebogo.mohale@raretag.co.za>
2023-02-19 08:57:31 +0200

    Dependencies upgrade

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import connectivity_plus_macos
-import device_info_plus_macos
+import connectivity_plus
+import device_info_plus
 import geolocator_apple
-import package_info_plus_macos
+import package_info_plus
 import path_provider_macos
 import shared_preferences_macos
 
