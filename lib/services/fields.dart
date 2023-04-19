34426389f2b18aa19fa9471bb848740df9e703c8
Zama Bongani Mbele <70573530+bonganimbele@users.noreply.github.com>
2023-04-19 10:41:25 +0200

    fields and keys update

--- lib/services/fields.dart
+++ lib/services/fields.dart
@@ -1,15 +1,16 @@
 part of 'package:mawa_package/mawa_package.dart';
 
 class Fields {
+  final String code;
+  Fields(this.code);
   static List fields = [];
   static Map singleField = {};
 
-  getFields() async {
+  static getFields() async {
     dynamic response =await NetworkRequests().securedMawaAPI(
       NetworkRequests.methodGet,
       resource: Resources.field,
     );
-    print('HERE');
       fields = await NetworkRequests.decodeJson(response, negativeResponse: []);
       return fields;
 
@@ -26,59 +27,32 @@ class Fields {
           JsonPayloads.fieldDescription : desc,
         });
 
-    // var url = Uri.https('api-dev.mawa.co.za:8181', '/mawa-api/resources/fields');
-    // var data = {
-    //   "code": code,
-    //   "description": description,
-    // };
-    //
-    // final response = await http.post(url, headers: {
-    //   'Content-Type': 'application/json',
-    //   'Accept': 'application/json',
-    //   'Authorization': 'Bearer ${NetworkRequests.token}',
-    // }, body: jsonEncode(data));
-    //
-    // var jsonResponse;
-    //
-    // if(response.statusCode == 200 || response.statusCode == 201) {
-    //   print(response.statusCode);
-    //   jsonResponse = jsonDecode(response.body);
-    //   print(jsonResponse);
-    //   print("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
-    // }
     return await NetworkRequests.decodeJson(response);
   }
 
   getSingleField() async {
-    fields = await NetworkRequests.decodeJson(
+    return await NetworkRequests.decodeJson(
         await NetworkRequests().securedMawaAPI(
           NetworkRequests.methodGet,
-          resource: Resources.field,
+          resource: '${Resources.field}/$code',
         ), negativeResponse: {});
+  }
+
+  mapAllFields() async {
+    fields = getFields();
+    Map mappedFields = {};
 
     for (int index = 0; index < fields.length;  index++) {
-      singleField['${fields[index][JsonResponses.fieldOptionDescription]}'] = fields[index][JsonResponses.fieldOptionCode];
+      mappedFields['${fields[index][JsonResponses.fieldOptionDescription]}'] = fields[index][JsonResponses.fieldOptionCode];
     }
-    return singleField;
+    return mappedFields;
+  }
 
+  getOptions() async {
+    return await NetworkRequests.decodeJson(
+        await NetworkRequests().securedMawaAPI(
+          NetworkRequests.methodGet,
+          resource: '${Resources.field}/$code/${Resources.option}',
+        ), negativeResponse: []);
   }
 }
-
-// part of mawa;
-//
-// class Fields {
-//   static late List fields;
-//   getFields() async {
-//     dynamic response = await NetworkRequests().securedMawaAPI(
-//       NetworkRequests.methodGet,
-//       resource: Resources.fields,
-//     );
-//
-//     if (NetworkRequests.statusCode == 200) {
-//       return response;
-//     } else {
-//       return [];
-//     }
-//   }
-// }
-//
