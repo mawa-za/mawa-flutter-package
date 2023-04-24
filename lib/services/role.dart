a4edc3ce7348e32332a409705c64a4c5b0f4cc7e
Zama Bongani Mbele <70573530+bonganimbele@users.noreply.github.com>
2023-04-24 09:35:52 +0200

    role update

--- lib/services/role.dart
+++ lib/services/role.dart
@@ -1,16 +1,53 @@
 part of 'package:mawa_package/mawa_package.dart';
 
 class Role{
+  final String role;
+  Role(this.role);
 
-  static dynamic roleWorkcenters = [];
-  static late List list = [];
 
-  getRoleWorkcenters({required String role}) async {
-    roleWorkcenters = {};
-    // List
-    list = await NetworkRequests.decodeJson(await NetworkRequests().securedMawaAPI(
+  static create({required String id, required String description}) async {
+    return await NetworkRequests().securedMawaAPI(
+        NetworkRequests.methodPost,
+        resource: Resources.role,
+      body: {
+          JsonPayloads.id: id,
+          JsonPayloads.description: description,
+      },
+    );
+  }
+
+  static getAll() async {
+    return await NetworkRequests.decodeJson(await NetworkRequests().securedMawaAPI(
         NetworkRequests.methodGet,
-        resource: '${Resources.role}/$role/workcenter'));
-    return list;
+      resource: Resources.role,
+    ),
+      negativeResponse: [],
+    );
+  }
+
+  getSpecific() async {
+    return await NetworkRequests.decodeJson(await NetworkRequests().securedMawaAPI(
+        NetworkRequests.methodGet,
+      resource: '${Resources.role}/$role',
+    ),
+      negativeResponse: {}
+    );
+  }
+
+  addWorkcenter({required List<String> workcenters}) async {
+    return await NetworkRequests().securedMawaAPI(
+        NetworkRequests.methodPost,
+        resource: '${Resources.role}/$role/${Resources.workcenter}',
+      body: workcenters,
+    );
+  }
+
+  getWorkcenters() async {
+    return await NetworkRequests.decodeJson(await NetworkRequests().securedMawaAPI(
+        NetworkRequests.methodGet,
+        resource: '${Resources.role}/$role/${Resources.workcenter}',
+    ),
+      negativeResponse: [],
+    );
   }
 }
