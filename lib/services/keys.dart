313500a467fd834a7e3ef533ccb4a2f539bc3104
Siboniso Ncube <siboniso.ncube@raretag.co.za>
2023-06-14 16:03:14 +0200

    Update path names class

--- lib/services/keys.dart
+++ lib/services/keys.dart
@@ -881,12 +881,12 @@ class FieldOptionTypes{
 
 }
 class PathNames{
-  static const String identityPath = 'identity';
-  static const String rolePath = 'role';
-  static const String contactPath= 'contact';
-  static const String addressPath= 'address';
-  static const String employmentPath= 'employment';
-  static const String employeePath= 'employee';
-  static const String customerPath= 'customer';
+  static const String identityPath = 'Identity';
+  static const String rolePath = 'Role';
+  static const String contactPath= 'Contact';
+  static const String addressPath= 'Address';
+  static const String employmentPath= 'Employment Details';
+  static const String employeePath= 'Employee Details';
+  static const String customerPath= 'Customer Details';
 
 }
