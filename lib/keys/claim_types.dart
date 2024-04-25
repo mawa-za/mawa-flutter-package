f8862dcf861f8ea0818a1c86cd32d3d66970d110
Zama Bongani Mbele <70573530+bonganimbele@users.noreply.github.com>
2024-04-25 13:40:09 +0200

    new claim type

--- lib/keys/claim_types.dart
+++ lib/keys/claim_types.dart
@@ -3,5 +3,6 @@ part of 'package:mawa_package/mawa_package.dart';
 class ClaimTypes{
   static const String cash = 'CASH';
   static const String funeral = 'FUNERAL';
+  static const String groupFuneral = 'FUNERAL';
   static const String tombstone = 'TOMBSTONE';
 }
