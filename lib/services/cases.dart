2dd3ef7010c71e3c3eaab54dee45e7a3cafb3c11
koketso.maponya <koketso.maponya@mawa.co.za>
2024-05-10 11:25:49 +0200

    -added json payloads -added the create method cases.dart modified json_payloads.dart modified

--- lib/services/cases.dart
+++ lib/services/cases.dart
@@ -23,6 +23,28 @@ class Cases {
     );
   }
 
+  static create({
+    required String client,
+    required String type,
+    required String court,
+    required String description,
+    required List applicants,
+    required List defendants,
+  }) async {
+    return await NetworkRequests().securedMawaAPI(
+      NetworkRequests.methodPost,
+      resource: Resources.case_,
+      body: {
+        JsonPayloads.client: client,
+        JsonPayloads.type: type,
+        JsonPayloads.court: court,
+        JsonPayloads.description: description,
+        JsonPayloads.applicants: applicants,
+        JsonPayloads.defendants: defendants,
+
+      },
+    );
+  }
 
 
 }
