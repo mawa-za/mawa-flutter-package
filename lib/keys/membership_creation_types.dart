8bbe2f61e1b38213d27b63285af046562aac15ea
Zama Bongani Mbele <70573530+bonganimbele@users.noreply.github.com>
2024-09-06 14:32:01 +0200

    Development (Memberships) Membership History Panel

---
+++ lib/keys/membership_creation_types.dart
@@ -0,0 +1,7 @@
+part of 'package:mawa_package/mawa_package.dart';
+
+class MembershipCreationTypes{
+  static const String transfer = 'TRANSFER';
+  static const String _new = 'New';
+  static const String upgrade = 'UPGRADE';
+}
